a8945a97eb72707142b1696e0dc8214638b041c3
fahrilmi25 <fahrilmi25@gmail.com>
2026-04-30 01:13:48 +0800

    Learning: Understanding module pattern

--- script.js
+++ script.js
@@ -1,60 +1,94 @@
+// //////////////////////////////////////////////
+// // EXPORTING & IMPORTING IN ES6 MODULES
+
+// // importing modules
+// // always put import modules at the top of the file
+// // this is strict by default, because it's modules
+// // import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
+// // & naming import
+// // addToCart('bread', 5)
+// // console.log(price, tq)
+// // console.log(shippingCost)
+
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log('importing modules');
+
+// // it's will like an object
+// ShoppingCart.addToCart('Bread', 7);
+// console.log(ShoppingCart.totalPrice);
+
+// // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// // console.log(price);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 90);
+// add('bread', 39);
+// add('spagetti', 30);
+
+// // modules with export & import have life connection
+// console.log(cart);
+
+// //////////////////////////////////////////////
+// // TOP LEVEL AWAIT ES2022
+
+// // console.log('START FETCHING');
+// // // top level await
+// // // await can work without async function, but it only inside module
+// // const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// // const data = await res.json();
+// // console.log(data)
+// // console.log('SOMETHING');
+
+// // real world example top level await
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+//   const data = await res.json();
+//   console.log(data);
+
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// // not very clean
+// // lastPost.then(last => console.log(last))
+
+// // not inside async function, but inside module (we can access it) = top level await
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2)
+
+// // top level await (without async function), will blocking execution code
+
 //////////////////////////////////////////////
-// EXPORTING & IMPORTING IN ES6 MODULES
+// THE MODULES PATTERN
 
-// importing modules
-// always put import modules at the top of the file
-// this is strict by default, because it's modules
-// import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
-// & naming import
-// addToCart('bread', 5)
-// console.log(price, tq)
-// console.log(shippingCost)
+// IFEE, Immediately invoked function expression
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrize = 237;
+  const totalQuantity = 23;
 
-import * as ShoppingCart from './shoppingCart.js';
-console.log('importing modules');
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart (shipping cost ${shippingCost})`);
+  };
 
-// it's will like an object
-ShoppingCart.addToCart('Bread', 7);
-console.log(ShoppingCart.totalPrice);
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
 
-// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-// console.log(price);
-import add, { cart } from './shoppingCart.js';
-add('pizza', 90);
-add('bread', 39);
-add('spagetti', 30);
+  return {
+    addToCart,
+    cart,
+    totalPrize,
+    totalQuantity,
+  };
+})();
 
-// modules with export & import have life connection
-console.log(cart);
+// closure : we can acces the variable throug the function where the variable declared
+// we cant acces the variable directly, but we can change it through the it's function
+ShoppingCart2.addToCart('pizza', 20)
+ShoppingCart2.addToCart('apple', 27)
 
-//////////////////////////////////////////////
-// TOP LEVEL AWAIT ES2022
-
-// console.log('START FETCHING');
-// // top level await
-// // await can work without async function, but it only inside module
-// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-// const data = await res.json();
-// console.log(data)
-// console.log('SOMETHING');
-
-// real world example top level await
-const getLastPost = async function () {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  const data = await res.json();
-  console.log(data);
-
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
-
-const lastPost = getLastPost();
-console.log(lastPost);
-
-// not very clean
-// lastPost.then(last => console.log(last))
-
-// not inside async function, but inside module (we can access it) = top level await
-const lastPost2 = await getLastPost();
-console.log(lastPost2)
-
-// top level await (without async function), will blocking execution code
+console.log(ShoppingCart2.shippingCost)
